fd387636506cec07d299dd1cac2e9eed806f3477
Lucas Masayuki Tamaribuchi <lutamaribuchi@gmail.com>
2023-06-28 20:46:19 -0300

    Initial commit

---
+++ components/app/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-footer app>
+        <v-container>
+            <v-row>
+                <v-col cols="12" class="text-center"> &copy; {{ new Date().getFullYear() }} Your Website Name </v-col>
+            </v-row>
+        </v-container>
+    </v-footer>
+</template>
